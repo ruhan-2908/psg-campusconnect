68ba9087a26f0d08e9ec7183844b914c955693c4
RUHAN K. B. <ruhankb29@gmail.com>
2025-11-20 14:45:27 +0530

    Idontlikechangesraa (#33)
    * Pull from dev
    
    * Add marksController and marksRoutes
    
    * Git Test
    
    * Update marksController.js
    
    * Update marksRoutes.js
    
    ---------
    
    Co-authored-by: nithish-github07 <nithi4527@gmail.com>

--- backend/controllers/marksController.js
+++ backend/controllers/marksController.js
@@ -1,13 +1,1 @@
-// GET /api/marks
-exports.getMarks = async (req, res) => {
-  res.json({
-    message: "Marks fetched successfully (starter code)",
-  });
-};
 
-// POST /api/marks/expected
-exports.saveExpectedMarks = async (req, res) => {
-  res.json({
-    message: "Expected marks saved (starter code)",
-  });
-};
